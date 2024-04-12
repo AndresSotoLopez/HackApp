69d3ae71b4ad8a45af9faf7a7d9fdbd9b7abf0f3
AndresSotoLopez <asotol22@fpcoruna.afundacion.org>
2024-04-12 19:50:34 +0200

    Schemas externalizados de los modelos

---
+++ Back-End/HackApp/hackapp_app/schemas/schemas_de_usuario.py
@@ -0,0 +1,44 @@
+class schemas():
+    
+    oAuthSchema = {
+        "type": "object",
+        "properties": {
+            "user": {"type": "string"},
+            "password": {"type": "string"}
+            },
+        "required": ["user", "password"]
+    }
+
+    oSchema = {
+        "type": "object",
+        "properties": {
+            "nombre": {"type": "string", "maxLength": 20},
+            "apellidos": {"type": "string", "maxLength": 50},
+            "username": {"type": "string", "maxLength": 50},
+            "biografia": {"type": ["string", "null"], "maxLength": 200},
+            "email": {"type": "string", "format": "email"},
+            "password": {"type": "string", "maxLength": 100},
+            "ct": {"type": "integer", "default": 0},
+            "telefono": {"type": "integer", "default": 999999999},
+            "avatar": {"type": ["string", "null"], "format": "uri", "maxLength": 100},
+            "posts": {"type": "integer", "default": 0, "minimum": 0},
+            "seguidores": {"type": "integer", "default": 0, "minimum": 0},
+            "seguidos": {"type": "integer", "default": 0, "minimum": 0},
+            "cuenta_privada": {"type": "boolean", "default": False},
+            "notificaciones": {"type": "boolean", "default": True},
+            "sms_code": {"type": ["integer", "null"], "default": 999999, "unique": True}
+        },
+        "required": ["nombre", "apellidos", "username", "email", "password"]
+    }
+
+    oCambioDatosSchema = {
+        "type": "object",
+        "properties": {
+            "token": {"type": "string"},
+            "username": {"type": "string"},
+            "contraseña": {"type": "string"},
+            "email": {"type": "string"},
+            "biografia": {"type": "string"}
+        },
+        "required": ["token"]
+    }
