b0d7477e25bf76bec6d94e83c75b45f3aa0c8f7a
AndresSotoLopez <asotol22@fpcoruna.afundacion.org>
2024-05-02 22:37:34 +0200

    Añadido recycler view al fragmento home

--- Front-End/app/src/main/java/com/app/hackapp/Home.java
+++ Front-End/app/src/main/java/com/app/hackapp/Home.java
@@ -1,64 +1,119 @@
 package com.app.hackapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Home#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Home extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private ImageButton btnNotis;
+    private EditText etSearch;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String sToken = "";
 
     public Home() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
-        Home fragment = new Home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Cargar el fragmento
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        // Setear las variables
+        recyclerView = view.findViewById(R.id.activity_home_exploits);
+        btnNotis = view.findViewById(R.id.activity_home_notis);
+        etSearch = view.findViewById(R.id.activity_home_search);
+
+
+        // Obtener el SharedPreferences
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        // Obtener el valor de una clave
+        sToken = sharedPreferences.getString("token", null);
+
+        if (sToken != null) {
+            peticion();
         }
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_fragment, container, false);
+    private void peticion(){
+
+        //Creacion de la lista para guardar los datos de la peticion
+        List<exploits> aExploits = new ArrayList<>();
+
+        //Creamos una peticion para obtener los datos del JSON
+        JsonArrayRequest request = new JsonArrayRequest
+                (Request.Method.GET,
+                        Server.getServer() + "v1/publicacion/0?tipoNoticia=2",
+                        null,
+                        new Response.Listener<JSONArray>(){
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                try {
+                                    //Recorremos el array de datos de la peticion
+                                    for (int nIndex = 0; nIndex < response.length(); nIndex++) {
+                                        JSONObject jsonObject = response.getJSONObject(nIndex);
+                                        exploits exploits = new exploits(jsonObject);
+                                        aExploits.add(exploits);
+                                    }
+
+                                    //Mostramos el recyclerview a traves de nuestro adapter
+                                    exploit_adapter adapter = new exploit_adapter(aExploits, getActivity());
+                                    recyclerView.setAdapter(adapter);
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(requireContext(), "Error: " + error, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                // Adjuntar el token en la cabecera de la solicitud
+                Map<String, String> headers = new HashMap<>();
+                headers.put("token", sToken);
+                return headers;
+            }
+        };
+
+        Volley.newRequestQueue(requireContext()).add(request);
     }
 }
